634a5312b8ff755b87b9d6f4ccbc7fb753927d0d
liushuai166 <liushuai166@ke.com>
2022-09-30 10:48:27 +0800

    feat:antialising

---
+++ src/camera/mod.rs
@@ -0,0 +1,36 @@
+use crate::color::vec3::Vec3;
+use crate::ray::Ray;
+
+pub struct Camera {
+    pub origin: Vec3,
+    pub lower_left_corner: Vec3,
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        const ASPECT_RATIO: f64 = 16.0 / 9.0;
+        const VIEWPORT_HEIGHT: f64 = 2.0;
+        const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+        const FOCAL_LENGTH: f64 = 1.0;
+
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+        let lower_left_corner =
+            origin - horizontal * 0.5 - vertical * 0.5 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
+        Camera {
+            origin: origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        let dir = self.lower_left_corner + self.horizontal * u + self.vertical * v - self.origin;
+        Ray::new(self.origin, dir)
+    }
+}
